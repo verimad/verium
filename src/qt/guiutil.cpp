a39cd85c08eaa0955637f95430f7a1792fe00b18
Douglas Pike <dpike@vericoin.info>
2016-08-04 22:59:51 -0400

    fix bug with copying private key

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -158,8 +158,10 @@ QString veriMenuStyleSheet = QString("QMenuBar { background-color: " + STR_COLOR
                             QMenu::item { background-color: transparent; margin: 0px 0px 4px 4px; padding: 4px 8px 4px 24px; } \
                             QMenu::item:selected { background-color: " + STR_COLOR + "; color: white; }");
 
+QString veriMessageBox = QString("QMessageBox { messagebox-text-interaction-flags: 5; }");
+
 // Put them all together
-QString veriStyleSheet = veriCentralWidgetStyleSheet + veriPushButtonStyleSheet + veriToolBarStyleSheet + veriToolTipStyleSheet + veriMiscStyleSheet + veriMenuStyleSheet;
+QString veriStyleSheet = veriCentralWidgetStyleSheet + veriPushButtonStyleSheet + veriToolBarStyleSheet + veriToolTipStyleSheet + veriMiscStyleSheet + veriMenuStyleSheet + veriMessageBox;
 
 
 // Special styling for AskPassphrasePage
