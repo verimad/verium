18347fc39c486196f00fe79df0133017baddb7fa
effectsToCause <dpike@vericoin.info>
2017-10-09 13:27:16 -0400

    simplify architecture specific assembly calls, enable avx2 support

--- src/scrypt.h
+++ src/scrypt.h
@@ -10,6 +10,7 @@
 
 static const int SCRYPT_SCRATCHPAD_SIZE = 134218239;
 static const int N = 1048576;
+#define USE_AVX2 = 1;
 
 int scrypt_best_throughput();
 
@@ -20,48 +21,37 @@ extern unsigned char *scrypt_buffer_alloc();
 extern "C" void scrypt_core(uint32_t *X, uint32_t *V, int N);
 extern "C" void sha256_transform(uint32_t *state, const uint32_t *block, int swap);
 
-#if defined(__x86_64__)
+#if defined(__x86_64__) && !defined(USE_AVX2)
 #define SCRYPT_MAX_WAYS 12
 #define HAVE_SCRYPT_3WAY 1
+#define HAVE_SHA256_4WAY 1
 #define scrypt_best_throughput() 3;
+extern "C" int sha256_use_4way();
+extern "C" void sha256_init_4way(uint32_t *state);
+extern "C" void sha256_transform_4way(uint32_t *state, const uint32_t *block, int swap);
 extern "C" void scrypt_core_3way(uint32_t *X, uint32_t *V, int N);
+#endif
 
-#if defined(USE_AVX2)
-#undef SCRYPT_MAX_WAYS
+#if defined(__x86_64__) && defined(USE_AVX2)
 #define SCRYPT_MAX_WAYS 24
 #define HAVE_SCRYPT_6WAY 1
+#define HAVE_SHA256_8WAY 1
+#define scrypt_best_throughput() 6;
+extern "C" int sha256_use_8way();
+extern "C" void sha256_init_8way(uint32_t *state);
+extern "C" void sha256_transform_8way(uint32_t *state, const uint32_t *block, int swap);
 extern "C" void scrypt_core_6way(uint32_t *X, uint32_t *V, int N);
 #endif
 
 #elif defined(__i386__)
 #define SCRYPT_MAX_WAYS 1
-#define scrypt_best_throughput() 1
-extern "C" void scrypt_core(uint32_t *X, uint32_t *V, int N);
-
-#elif defined(__arm__) && defined(__APCS_32__)
-extern "C" void scrypt_core(uint32_t *X, uint32_t *V, int N);
-
-#if defined(__ARM_NEON__)
-#undef HAVE_SHA256_4WAY
-#define SCRYPT_MAX_WAYS 1
-#define HAVE_SCRYPT_3WAY 0
-#define scrypt_best_throughput() 1
-#endif
-#endif
-#if defined(__ARM_NEON__) || defined(__ALTIVEC__) || defined(__i386__) || defined(__x86_64__)
 #define HAVE_SHA256_4WAY 1
+#define scrypt_best_throughput() 1
+extern "C" void scrypt_core(uint32_t *X, uint32_t *V, int N);
 extern "C" int sha256_use_4way();
 extern "C" void sha256_init_4way(uint32_t *state);
 extern "C" void sha256_transform_4way(uint32_t *state, const uint32_t *block, int swap);
 #endif
-
-#if defined(__x86_64__) && defined(USE_AVX2)
-#define HAVE_SHA256_8WAY 1
-extern "C" int sha256_use_8way();
-extern "C" void sha256_init_8way(uint32_t *state);
-extern "C" void sha256_transform_8way(uint32_t *state, const uint32_t *block, int swap);
-#endif
-
 
 #define bswap_32(x) ((((x) << 24) & 0xff000000u) | (((x) << 8) & 0x00ff0000u) \
                    | (((x) >> 8) & 0x0000ff00u) | (((x) >> 24) & 0x000000ffu))
@@ -70,4 +60,3 @@ static inline uint32_t swab32(uint32_t v)
 {
     return bswap_32(v);
 }
-#endif
