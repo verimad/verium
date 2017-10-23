814ada0aa90f1a338ffd8a1bd5818573f33955aa
effectsToCause <dpike@vericoin.info>
2017-10-23 17:00:48 -0400

    update linux build file 64bit

--- release/linux_setup_builder.sh
+++ release/linux_setup_builder.sh
@@ -3,18 +3,18 @@
 # This script depends on the GNU script makeself.sh found at: http://megastep.org/makeself/
 # Note: The structure of this package depends on the -rpath,./lib to be set at compile/link time.
 
-version="1.0.0"
+version="1.1.0"
 arch=`uname -i`
 
 if [ "${arch}" = "x86_64" ]; then
     arch="64bit"
-    QtLIBPATH="${HOME}/Qt/5.4/gcc_64"
+    QtLIBPATH="${HOME}/programs/Qt/5.5/gcc_64"
 else
     arch="32bit"
     QtLIBPATH="${HOME}/Qt/5.4/gcc"
 fi
 
-if [ -f verium-qt ] && [ -f verium.conf ] && [ -f README ]; then
+if [ -f verium-qt ] && [ -f verium.conf ]; then
     echo "Building Verium_${version}_${arch}.run ...\n"
     if [ -d Verium_${version}_${arch} ]; then
         rm -fr Verium_${version}_${arch}/
@@ -25,7 +25,6 @@ if [ -f verium-qt ] && [ -f verium.conf ] && [ -f README ]; then
     mkdir Verium_${version}_${arch}/imageformats
     cp verium-qt Verium_${version}_${arch}/
     cp verium.conf Verium_${version}_${arch}/
-    cp README Verium_${version}_${arch}/
     ldd verium-qt | grep libssl | awk '{ printf("%s\0", $3); }' | xargs -0 -I{} cp {} Verium_${version}_${arch}/libs/
     ldd verium-qt | grep libdb_cxx | awk '{ printf("%s\0", $3); }' | xargs -0 -I{} cp {} Verium_${version}_${arch}/libs/
     ldd verium-qt | grep libboost_system | awk '{ printf("%s\0", $3); }' | xargs -0 -I{} cp {} Verium_${version}_${arch}/libs/
@@ -35,7 +34,7 @@ if [ -f verium-qt ] && [ -f verium.conf ] && [ -f README ]; then
     ldd verium-qt | grep libminiupnpc | awk '{ printf("%s\0", $3); }' | xargs -0 -I{} cp {} Verium_${version}_${arch}/libs/
     ldd verium-qt | grep libqrencode | awk '{ printf("%s\0", $3); }' | xargs -0 -I{} cp {} Verium_${version}_${arch}/libs/
     cp ${QtLIBPATH}/lib/libQt*.so.5 Verium_${version}_${arch}/libs/
-    cp ${QtLIBPATH}/lib/libicu*.so.53 Verium_${version}_${arch}/libs/
+    cp ${QtLIBPATH}/lib/libicu*.so.54 Verium_${version}_${arch}/libs/
     cp ${QtLIBPATH}/plugins/platforms/lib*.so Verium_${version}_${arch}/platforms/
     cp ${QtLIBPATH}/plugins/imageformats/lib*.so Verium_${version}_${arch}/imageformats/
     strip Verium_${version}_${arch}/verium-qt
@@ -46,11 +45,10 @@ if [ -f verium-qt ] && [ -f verium.conf ] && [ -f README ]; then
     if [ -f Verium_${version}_${arch}.run ]; then
         rm -f Verium_${version}_${arch}.run
     fi
-    makeself.sh --notemp Verium_${version}_${arch} Verium_${version}_${arch}.run "\nCopyright (c) 2014-2015 The Verium Developers\nVerium will start when the installation is complete...\n" ./verium-qt \&
+    makeself.sh --notemp Verium_${version}_${arch} Verium_${version}_${arch}.run "\nCopyright (c) 2014-2017 The Verium Developers\nVerium will start when the installation is complete...\n" ./verium-qt \&
     sudo rm -fr Verium_${version}_${arch}/
     echo "Package created in: $PWD/Verium_${version}_${arch}.run\n"
 else
     echo "Error: Missing files!\n"
     echo "Copy this file to a setup folder along with verium-qt, verium.conf and README.\n"
 fi
-
